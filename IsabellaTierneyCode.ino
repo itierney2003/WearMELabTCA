bd5ddb4060561b89f893f5eea27940cc0451552a
itierney2003 <122463279+itierney2003@users.noreply.github.com>
2024-06-07 11:04:07 -0400

    Update IsabellaTierneyCode.ino

--- IsabellaTierneyCode.ino
+++ IsabellaTierneyCode.ino
@@ -1,6 +1,7 @@
 //goal of this code to to determine rpm of encoder
 //know that the PPR is 17
 //knowing this, record the time every 17 PPR to obtain seconds per revolution
+//also use a potentiometer to use PWM to control the speed
 
 #define IN1 7
 #define IN2 8
@@ -8,20 +9,26 @@
 #define EN_A 2
 #define EN_B 3
 
-volatile int pulse_count = 0 ;
-int current_pulse_count = 0;
-int previous_pulse_count = 0;
+int potentiometer = 0; //value of analog value read from the potentiometer
+int power = 0; //signal sent to the enable pin
+
+volatile double pulse_count = 0 ;
+double previous_pulse_count = 0;
 
 //Pulses Per Revolution of the motor
 const int PPR = 17;
 
 long previousMillis = 0;
 long currentMillis = 0;
-float time_interval = 0; //
-float position_interval = 0;
+long previousMillisMain = 0;
+float time_interval = 0; //currentMillis - previousMillis
+float position_interval = 0; //pulse_count - previous_pulse_count //should always be 17
+
+float eprev = 0; //previous error
+float eintegral = 0; //error integral
 
 void setup() {
-  Serial.begin(9600); //Set the band rate to your Bluetooth module.
+  Serial.begin(115200); //Set the band rate to your Bluetooth module.
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(PWM, OUTPUT);
@@ -32,52 +39,69 @@ void setup() {
 }
 
 void loop() {
-  
+
+  //start timer
+  currentMillis = millis();
+
+  //power
+  potentiometer = analogRead(A0);
+  power = map(potentiometer, 0, 1023, 0, 255);
+
+  //target speed
+  float target_speed = desired_speed();
+
   //speed of enocoder
   float current_speed = read_speed();
 
-  //target speed
-  int target_speed = 10000; //motor rated speed in RPM
+  //PID Constants
+  float kp = 1;
+  float kd = 0;
+  float ki = 0;
+
+  float deltaT = ((float)(currentMillis-previousMillisMain))/1.0e6;
+  previousMillisMain = currentMillis;
 
   //error
   int e = target_speed - current_speed;
 
-  //motor power
-  //float power = e;
-  //if(power > 255) {
-  //  power = 255;    
-  //}
+  //derivative
+  float dedt = (e-eprev)/(deltaT);
+
+  //integral
+  eintegral = eintegral + e*deltaT;
+
+  //control signal
+  float u = kp*e + kd*dedt + ki*eintegral;
+
+  power = fabs(u);
+    if( power > 255 ) {
+    power = 255;
+  }
 
   //signal to motor
-  motorControl(200, 1);
-  
-  Serial.print("Position: ");
-  Serial.print(pulse_count);
-  Serial.print("  Previous Position: ");
-  Serial.print(previous_pulse_count);
-  Serial.print("  Position Interval: ");
-  Serial.print(position_interval);
-  Serial.print("  Time Interval: ");
-  Serial.print(time_interval);
-  Serial.print("  Current Time: ");
+  motorControl(power, 1);
+
   Serial.print(currentMillis);
-  Serial.print("  Current Speed: ");
-  Serial.println(current_speed);
+  Serial.print(",");
+  Serial.print(current_speed);
+  Serial.print(",");
+  Serial.print(target_speed);
+  Serial.print("  PWM Power ");
+  Serial.println("power");
 
 }
 
 //interrupt service routine that increments or decrements the position count
 //when encoder A produces an interrupt because a rising edge is detected, encoder B is used to determine if the position is moving forward or backwards
 void StartInterruptA(){
+
   int b = digitalRead(EN_B);
+
   if (b > 0) {
     pulse_count++;
-    if(pulse_count%17 == 0) {
-      currentMillis = millis();
+    if((int)pulse_count%17 == 0) {
       time_interval = currentMillis - previousMillis;
       previousMillis = currentMillis;
-      position_interval = pulse_count - previous_pulse_count;
-      previous_pulse_count = pulse_count;
     }
   }
   else{
@@ -88,8 +112,10 @@ void StartInterruptA(){
 //function with two arguments; power and dirction
 //the value of the power is written to the enable pin of the motor drive using PWM
 //direction is used to tell the motor which way to spin
-void motorControl(int pwr, int direct) {
-  analogWrite(PWM, pwr);
+void motorControl(int power, int direct) {
+
+  analogWrite(PWM, power);
+
   if (direct == 1) {
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
@@ -104,9 +130,17 @@ void motorControl(int pwr, int direct) {
 //when the pulse count = PPR (17 in this case), records the time to complete one revolution
 //calculations to determine the RPM from miliseconds per revolution
 float read_speed(void) {
-  //current_pulse_count = pulse_count;
   float RPM;
     //(RPM = (1/ms)*(60s/min)*(10e3ms/s)
     RPM = (1/time_interval)*60*1e3;
     return RPM;
+}
+
+//function to calculate the desired speed based on the set potentiometer value 
+float desired_speed(void) {
+  float speed;
+  //max PWM is 255
+  //whatever the power is (0-255)*rated speed of motor is the speed
+  speed = (power/255)*10000;
+  return speed;
 }
